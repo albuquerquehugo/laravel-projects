83358c5291f4c6c2e9ab6b51673166496f8a4b87
Hugo Albuquerque <albuquerquehugo@gmail.com>
2024-05-20 22:14:31 -0400

    Laravel-React tutorial of a CRUD application

---
+++ laravel-react-fullstack/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::apiResource('/users', UserController::class)->middleware('auth:sanctum');
