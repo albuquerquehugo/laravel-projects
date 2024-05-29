968dfb006ad8a2e50ba14125302ff9c53e05b0d1
Hugo Albuquerque <albuquerquehugo@gmail.com>
2024-05-28 23:00:29 -0400

    Laravel + React + Stripe tutorial. First working commit, website built with Features 1 & 2 and Stripe integrated.

---
+++ laravel11-react-saas/resources/js/Pages/Credit/Index.jsx
@@ -0,0 +1,50 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+import PackagesPricingCards from '@/Components/PackagesPricingCards';
+
+export default function Index({ auth, packages, features, success, error }) {
+    const available_credits = auth.user.available_credits;
+
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                    Your credits
+                </h2>
+            }
+        >
+            <Head title="Your credits" />
+
+            <div className='py-12'>
+
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    {success &&
+                        <div className='rounded-lg bg-emerald-500 text-gray-100 p-3 mb-4'>
+                            {success}
+                        </div>
+                    }
+                    {error &&
+                        <div className='rounded-lg bg-red-500 text-gray-100 p-3 mb-4'>
+                            {error}
+                        </div>
+                    }
+
+                    <div className='bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg relative'>
+                        <div className='flex flex-col gap-3 items-center p-4'>
+                            <img
+                                src="/img/coin.png"
+                                className="w-[100px]"
+                                alt=""
+                            />
+                            <h3 className='text-white text-2xl'>
+                                You have {available_credits} credits.
+                            </h3>
+                        </div>
+                    </div>
+                    <PackagesPricingCards packages={packages.data} features={features.data} />
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
